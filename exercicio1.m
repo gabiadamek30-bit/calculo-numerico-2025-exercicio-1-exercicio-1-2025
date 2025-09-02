47bd8e52fd8d35c4093848b301780e85a626f6f7
Maria Gabriela von Adamek <gabiadamek30@gmail.com>
2025-09-01 22:30:54 -0300

    exercicio

--- exercicio1.m
+++ exercicio1.m
@@ -6,9 +6,19 @@ imax = 20;
 % nao alterar: fim
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
+t_roots = zeros(imax,1);
+t_roots(1)= x0;
 
-% digite seu codigo aqui
-t = 0; % alterar
+for ii = 1:lenght(t_roots)= 1
+    if ii ~= 1
+      erro(ii) = abs((t_roots(ii)-t_roots(ii-1))/t_roots(ii));
+        if erro(ii)<es
+            break
+        endif
+    endif
+    t_roots(ii+1) = t_roots(ii) - func(t_roots(ii))/func_d(t_roots((ii));
+endfor
+t=t(ii)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
